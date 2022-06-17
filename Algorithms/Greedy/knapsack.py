4f1603913d74031353cd57a8e7ed886acc872a2e
tnw012 <trevin_wolfe@hotmail.com>
2022-06-17 15:30:34 -0500

    finished fractional greedy algorithm

--- Algorithms/Greedy/knapsack.py
+++ Algorithms/Greedy/knapsack.py
@@ -1,4 +1,6 @@
+from audioop import reverse
 from dataclasses import dataclass
+from operator import attrgetter
 import random
 
 @dataclass
@@ -12,44 +14,48 @@ class Item :
         return string
 
 class Knapsack:
+    MAX_CAPACITY = 30
 
-    # Class Variabless
-    MAX_WEIGHT = 30
-
-    def __init__(self, size=10):
-        self.knapsack = list()
-        self.__item_list = list()
-        self.__total_items = 0
-        self.__size = size
+    def __init__(self):
+        self.__knapsack = list()
+        self.__total_value = 0
         self.__total_weight = 0
+        self.__capacity = self.MAX_CAPACITY
 
     def __str__(self):
-        string = f"item list    : {self.__item_list}\n" \
-        f"total number of items : {self.__total_items}\n" \
-        f"total weight          : {self.__total_weight}\n"
-        
+        self.__total_value = float("%.2f" % self.__total_value)
+
+        string = f"Items in knapsack: {self.__knapsack}\n" \
+        f"Total value of knapsack: {self.__total_value}\n" \
+        f"Total weight of knapsack: {self.__total_weight}"        
         return string
+    
+    # Algorithm for the Knapsack Problem
+    def dynamic(self, input_list):
+        pass
 
-    def __generate_item(self):
-        random_value = random.randint(50, 150)
-        random_weight = random.randint(1, 30) 
+    # Fractional Greedy Algorithm for the Knapsack Problem
+    def fractional_greedy(self, input_list):
+        fractional_value = 0.0
+        self.__knapsack.clear()
 
-        GeneratedItem = Item(random_value, random_weight) 
+        input_list.sort(key=attrgetter("value"), reverse=True)
 
-        return GeneratedItem
+        index = 0
 
-    def generate_list(self):
-        for i in range(self.__size):
-            item = self.__generate_item()
-            self.__item_list.append(item)
+        while (self.__total_weight < self.MAX_CAPACITY) or (self.__capacity > 0):
 
-    def find_optimal(self):
+            if input_list[index].weight > self.__capacity:
+                fractional_value = (self.__capacity / input_list[index].weight) * input_list[index].value
+                
+                self.__total_value += fractional_value
 
-        self.__generate_list()
-
-        for index in self.__item_list:
-            if self.__total_weight > self.MAX_WEIGHT:
-                print("Knapsack has gone over weight capacity.")
-                return None
+                input_list[index].weight = self.__capacity
             else:
-                pass
+                self.__total_value  += input_list[index].value
+                
+            self.__capacity -= input_list[index].weight
+            self.__total_weight += input_list[index].weight
+
+            self.__knapsack.append(input_list[index])
+            index += 1
