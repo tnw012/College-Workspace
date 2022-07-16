a72551da7720819e3ddeaf2dc76fd7d1b46fb6b3
tnw012 <trevin_wolfe@hotmail.com>
2022-07-15 19:20:12 -0500

    refined algorithms

--- Algorithms/NP-Completeness/bin_packing.py
+++ Algorithms/NP-Completeness/bin_packing.py
@@ -4,17 +4,17 @@ def next_fit(num_list, capacity):
     
     for index in range(len(num_list)):
         
-        if remainder >= num_list[index]:
-            remainder -= num_list[index]
-        else:
+        if num_list[index] > remainder:
             bin_count += 1
             remainder = capacity - num_list[index]
+        else:
+            remainder -= num_list[index]
             
     return bin_count
 
 def best_fit(num_list, capacity, length):
     bin_count = 0
-    room_left = [0] * length
+    bin_remainder = [0] * length
     
     for i in range(length):
         j = 0
@@ -23,18 +23,33 @@ def best_fit(num_list, capacity, length):
         bin = 0
         
         for j in range(bin_count):
-            if room_left[j] >= num_list[i] and (room_left[j] - num_list[i] < min_space):
+            if bin_remainder[j] >= num_list[i] and (bin_remainder[j] - num_list[i] < min_space):
                 bin = j 
-                min_space = room_left[j] - num_list[i]
+                min_space = bin_remainder[j] - num_list[i]
                 
         if (min_space == capacity + 1):
-            room_left[bin_count] = capacity - num_list[i]
+            bin_remainder[bin_count] = capacity - num_list[i]
             bin_count += 1
         else:
-            room_left[bin] -= num_list[i]
+            bin_remainder[bin] -= num_list[i]
             
     return bin_count
-            
+
+def first_fit(num_list, capacity, length):
+    bin_count = 0
+    bin_remainder = [0] * length
+    
+    for i in range(length):
+        j = 0
         
+        while j < bin_count:
+            if (bin_remainder[j] >= num_list[i]):
+                bin_remainder[j] -= num_list[i]
+                break
+            j += 1
+            
+        if (j == bin_count):
+            bin_remainder[bin_count] = capacity - num_list[i]
+            bin_count += 1
     
     return bin_count
--- Algorithms/NP-Completeness/test.py
+++ Algorithms/NP-Completeness/test.py
@@ -21,7 +21,7 @@ def test_binpacking(num_list, capacity, test_number=100):
     average_time = 0.0
         
     # loop twice to test both algorithm
-    for index in range(2):
+    for index in range(3):
 
         if index == 0:
             print("Testing NP-Completeness next-fit:")
@@ -29,10 +29,29 @@ def test_binpacking(num_list, capacity, test_number=100):
             # loop from 0 to test_number(number of tests)
             for i in range(test_number):
                 start_time = time.perf_counter()
-            
-                
+        
                 next_bin_total = bp.next_fit(num_list, capacity)
+                
+                end_time = time.perf_counter()
+
+                # calculate the total time in milliseconds then append time to a time array.
+                run_time = (end_time - start_time) * 1000
+                time_array.append(run_time)
+
+            # calculate the average time during the test.
+            average_time = calculate_average_time(time_array)
+
+            print("The average time out of %i tests = %.4f ms" % (test_number, average_time))
+            print("next-fit number of bins required: ", next_bin_total, "\n")
             
+        elif (index == 1):
+            print("Testing NP-Completeness first-fit:")
+
+            # loop from 0 to test_number(number of tests)
+            for i in range(test_number):
+                start_time = time.perf_counter()
+                
+                next_bin_total = bp.first_fit(num_list, capacity, len(num_list))
             
                 end_time = time.perf_counter()
 
@@ -44,9 +63,10 @@ def test_binpacking(num_list, capacity, test_number=100):
             average_time = calculate_average_time(time_array)
 
             print("The average time out of %i tests = %.4f ms" % (test_number, average_time))
-            print("next-fit number of bins: ", next_bin_total)
+            print("first-fit number of bins required: ", next_bin_total, "\n")
+            
         else:
-            print("\nTesting NP-Completeness best-fit:")
+            print("Testing NP-Completeness best-fit:")
             
             # loop from 0 to test_number(number of tests)
             for i in range(test_number):
@@ -65,4 +85,4 @@ def test_binpacking(num_list, capacity, test_number=100):
             average_time = calculate_average_time(time_array)
 
             print("The average time out of %i tests = %.4f ms" % (test_number, average_time))
-            print("best-fit number of bins required: ", best_bin_total)
+            print("best-fit number of bins required: ", best_bin_total, "\n")
