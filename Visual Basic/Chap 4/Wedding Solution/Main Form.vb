eae91d581e62187a21ba873b7d15d817f3e59035
tnw012 <72952518+tnw012@users.noreply.github.com>
2023-07-20 18:26:53 -0500

    Add files via upload

---
+++ Visual Basic/Chap 4/Wedding Solution/Main Form.vb
@@ -0,0 +1,20 @@
+﻿' Name:         Wedding Project
+' Purpose:      Displays the number of tables needed.
+' Programmer:   <your name> on <current date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculate and display the number of required tables.
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+End Class
---
+++ Visual Basic/Chap 4/You Do It 5 Solution/Main Form.vb
@@ -0,0 +1,40 @@
+﻿' Name:         You Do It 5 Project
+' Purpose:      Displays a shipping charge.
+' Programmer:   <your name> on <currrent date>
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        ' Displays a shipping charge.
+
+        Dim dblPurchase As Double
+        Dim dblShip As Double
+
+        Double.TryParse(txtPurchase.Text, dblPurchase)
+
+        lblShipping.Text = dblShip.ToString("C2")
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtCode_Enter(sender As Object, e As EventArgs) Handles txtCode.Enter
+        txtCode.SelectAll()
+    End Sub
+
+    Private Sub txtCode_TextChanged(sender As Object, e As EventArgs) Handles txtCode.TextChanged
+        lblShipping.Text = String.Empty
+    End Sub
+
+    Private Sub txtPurchase_Enter(sender As Object, e As EventArgs) Handles txtPurchase.Enter
+        txtPurchase.SelectAll()
+    End Sub
+
+    Private Sub txtPurchase_TextChanged(sender As Object, e As EventArgs) Handles txtPurchase.TextChanged
+        lblShipping.Text = String.Empty
+    End Sub
+End Class
